d3e150c6f26bf80337182455c6c2733be33c3459
aaron k saunders <aaron@clearlyinnovative.com>
2017-08-17 15:54:17 +0200

    starting to integrate mobx solution

---
+++ src/personStore.js
@@ -0,0 +1,40 @@
+import { observable, action, computed, extendObservable, useStrict } from 'mobx';
+// this allows us to use redux-dev-tools
+import remotedev from 'mobx-remotedev';
+useStrict(true);
+
+
+// this allows us to use redux-dev-tools
+//@remotedev({})
+class PersonStore {
+  constructor() {
+    extendObservable(this, {
+
+      persons:[],
+
+      get: action(function get(_personIdx) {
+        return this.persons[_personIdx];
+      }),
+
+      add: action(function add(_person) {
+        this.persons.push({ name: _person });
+      }),
+
+      remove: action(function remove(_personIdx) {
+        this.persons.splice(_personIdx, 1)
+      })
+
+    })
+  }
+
+
+ // @computed get personList() {
+ //   return this.persons
+ // }
+
+  toJS() {
+    return this.persons.map(p => p.toJS());
+  }
+}
+
+export default remotedev(PersonStore);
