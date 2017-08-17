0001cc24a0655ea4a4dcaa84fff19a1353c70d77
aaron k saunders <aaron@clearlyinnovative.com>
2017-08-17 16:48:13 +0200

    update of readme

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@
 // https://reacttraining.com/react-router/web/guides/philosophy
 //
 
-import React  from 'react';
+import React from 'react';
 import { render } from 'react-dom'
 import './style.css';
 
 import { Provider } from 'mobx-react';
 import PersonStore from './personStore';
-import RootComponent  from './components/RootComponent'
+import RootComponent from './components/RootComponent'
 
 
 // create the appState using the person store
